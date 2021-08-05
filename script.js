459a63d8fed1ed010ec40438c42a7ae30532703c
dakkuti <dianarojas3245@gmail.com>
2021-08-05 18:53:09 -0500

    Agregando al repositorio

---
+++ script.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector('.nav-link.text-white');
+
+nav.addEventListener('click',function(){
+    nav.style="text-decoration-line: revert";
+    nav.style.Color="#F25F4C!important";
+    nav.style="text-underline-position: under";
+})
